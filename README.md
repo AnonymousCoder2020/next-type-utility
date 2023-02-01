36fb6066418437ae97f9e7dd51516d8f44cd19a0
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2023-02-01 23:22:16 +0900

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# next-type-utility" 
+# next-type-utility
+
+```
+yarn add -D https://github.com/AnonymousCoder2020/next-type-utility
+```
---
+++ index.ts
@@ -0,0 +1,36 @@
+// Lang
+export type OrArr<T> = T | T[]
+export type OrPromise<T> = T | Promise<T>
+export type SplitStr<T extends string, S extends string> = T extends `${infer A}${S}${infer B}` ? [A, ...SplitStr<B, S>] : [T]
+export type SplitComma<T extends string> = SplitStr<T, '.'>
+export type AccessByPath<T extends PlainAnyObj, P extends string[]> = P extends [infer A, ...infer B]
+  ? A extends keyof T
+    ? B extends string[]
+      ? AccessByPath<T[A], B>
+      : T[A]
+    : never
+  : T
+export type AccessByCommaPath<T extends PlainAnyObj, P extends string> = AccessByPath<T, SplitComma<P>>
+export type IsAsyncFunction<F extends AnyFunc> = F extends (...args: unknown[]) => Promise<unknown> ? true : false
+
+// Obj
+export type EmptyObj = { [key: string]: never }
+export type FillObj<K extends string, V> = { [P in K]: V }
+export interface PlainObj<T> {
+  [key: string]: T
+}
+export type PlainAnyObj = PlainObj<any>
+export type ExtractVals<T extends PlainAnyObj, U> = {
+  [P in keyof T]: Extract<T[P], U>
+}
+
+// Func
+export type AnyFunc = (...args: any[]) => any
+export type OrAsyncFunc<F extends AnyFunc> = (...args: Parameters<F>) => OrPromise<ReturnType<F>>
+
+// Class
+export type ClassProps<C> = { [P in keyof C]: C[P] extends Function ? never : C[P] }
+export type ClassPropsPartial<C> = Partial<ClassProps<C>>
+
+// RegExp
+export type MatchResult = RegExpMatchArray | null
